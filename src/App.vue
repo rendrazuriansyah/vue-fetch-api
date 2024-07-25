8b3098a9a888bf051ad9001588ca17578cf39214
twingqywingqy <rendra.zurd04@gmail.com>
2024-07-25 11:27:04 +0700

    feat: setup serverless Restful API with json-server

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-	<RouterLink to="/">Home</RouterLink>
-	<RouterLink to="/about">About</RouterLink>
 	<RouterView />
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,64 +1,11 @@
 <script setup>
 import ProductCard from "@/components/ProductCard.vue";
 import Pagination from "@/components/Pagination.vue";
-import {
-	onBeforeMount,
-	onBeforeUnmount,
-	onBeforeUpdate,
-	onMounted,
-	onUnmounted,
-	onUpdated,
-	ref,
-} from "vue";
-
-// Lifecycle hook onBeforeMount dipanggil sebelum komponen dimount ke DOM.
-// Digunakan untuk melakukan operasi asinkron yang perlu dilakukan sebelum komponen dirender.
-// Di sini saya menggunakan setTimeout untuk menyalakan operasi asinkron dan mencetak pesan ke konsol.
-onBeforeMount(async () => {
-	await new Promise((resolve) => setTimeout(resolve, 1000)); // Tunggu 1 detik
-	console.log("Komponen sedang dimount"); // Cetak pesan ke konsol
-});
-
-// Lifecycle hook onMounted dipanggil setelah komponen dimount ke DOM.
-// Digunakan untuk melakukan operasi yang perlu dilakukan setelah komponen dirender.
-// Di sini saya menggunakan setTimeout untuk mengundurkan waktu pencetakan pesan ke konsol.
-onMounted(() => {
-	setTimeout(() => {
-		console.log("Komponen telah dimount"); // Cetak pesan ke konsol setelah 2 detik
-	}, 2000);
-});
-
-// Lifecycle hook onBeforeUpdate dipanggil sebelum komponen diperbarui.
-// Digunakan untuk melakukan operasi yang perlu dilakukan sebelum komponen dirender ulang.
-// Di sini saya menggunakan setTimeout untuk menyalakan operasi asinkron dan mencetak pesan ke konsol.
-onBeforeUpdate(async () => {
-	await new Promise((resolve) => setTimeout(resolve, 1000)); // Tunggu 1 detik
-	console.log("Komponen sedang diperbarui"); // Cetak pesan ke konsol
-});
-
-// Lifecycle hook onUpdated dipanggil setelah komponen diperbarui.
-// Digunakan untuk melakukan operasi yang perlu dilakukan setelah komponen dirender ulang.
-// Di sini saya menggunakan setTimeout untuk mengundurkan waktu pencetakan pesan ke konsol.
-onUpdated(async () => {
-	await new Promise((resolve) => setTimeout(resolve, 2000)); // Tunggu 2 detik
-	console.log("Komponen telah diperbarui"); // Cetak pesan ke konsol setelah 2 detik
-});
-
-// Lifecycle hook onBeforeUnmount dipanggil sebelum komponen di-unmount dari DOM.
-// Digunakan untuk melakukan operasi yang perlu dilakukan sebelum komponen dihapus.
-// Di sini saya menggunakan setTimeout untuk menyalakan operasi asinkron dan mencetak pesan ke konsol.
-
-const page = ref(1);
-
-function nextPage() {
-	page.value++;
-}
+import { ref } from "vue";
 </script>
 
 <template>
 	<main>
-		{{ page }}
-		<button @click="nextPage">Next</button>
 		<div class="product-grid">
 			<ProductCard />
 		</div>
